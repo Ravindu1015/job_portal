ded77c3071c9fdf79efc956ba58a93ce192d874f
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-10-21 23:53:27 +0530

    Refactor server.js to use ES modules and update environment variables

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 //imports
 //const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
+
+//dot env config
+dotenv.config();
 
 //rest object
 const app = express();
@@ -10,7 +14,12 @@ app.get('/', (req, res) => {
     res.send("<h1>Hi !Welcome to Job -Portal</h1>");
 });
 
+//port
+const PORT = process.env.PORT || 8080;
+
 //listen
 app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+    console.log("Server is running on ${process.env.DEV_MODE} port 8080"
+        
+    );
 });
