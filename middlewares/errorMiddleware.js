47d6d2a385ef8fd5d787cb460f98ec6bb557ee85
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-10-26 19:57:23 +0530

    Refactor errorMiddleware.js to handle missing field and duplicate errors

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,10 +2,30 @@
 const errorMiddleware = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
+  const defaultErrors = {
+    statusCode: 500,
+    message: "Internal Server Error",
+  };
+
   res.json({
     message: error.message,
     stack: process.env.DEV_MODE === "true" ? "stack" : null,
   });
+  //missing field error
+  if (error.name === "ValidationError") {
+    defaultErrors.statusCode = 400;
+    defaultErrors.message = Object.values(error.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+
+  //duplicate error
+  if (error.code === 11000) {
+    defaultErrors.statusCode = 400;
+    defaultErrors.message = "Duplicate field value entered";
+  }
+
+  res.status(defaultErrors.statusCode).json({ message: defaultErrors.message });
 };
 
 export default errorMiddleware;
