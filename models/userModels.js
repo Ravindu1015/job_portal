4aa6adb1bccaa18496aa0ff3bc28a816d1e85ef3
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-10-28 23:26:41 +0530

    Refactor userModels.js to include JWT token creation

--- models/userModels.js
+++ models/userModels.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
 // Define the schema
 const userSchema = new mongoose.Schema(
@@ -38,5 +39,12 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+//json web token
+userSchema.methods.createJWT = function () {
+  return JWT.sign({ userid: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 export default mongoose.model("User", userSchema);
