4586ff4ac90a0114da0269314b9de5b845aff8bd
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-10-29 23:59:05 +0530

    Refactor userModels.js to include password hashing and JWT token creation

--- models/userModels.js
+++ models/userModels.js
@@ -34,23 +34,27 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-//middlewares
-userSchema.pre("save", async function () {
+// Middleware to hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // Only hash if password is new/modified
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-//compare password
+// Compare password method
 userSchema.methods.comparePassword = async function (userPassword) {
-  const isMatch = await bcrypt.compare(userPassword, this.password);
-  return isMatch;
+  return await bcrypt.compare(userPassword, this.password);
 };
 
-//json web token
+// JWT creation method
 userSchema.methods.createJWT = function () {
-  return JWT.sign({ userid: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
+  return JWT.sign(
+    { userid: this._id },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE || "1d" } // Fallback to 1 day if env not set
+  );
 };
 
 export default mongoose.model("User", userSchema);
