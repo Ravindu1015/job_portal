4a11ca32aa0051171be336978e369b3945942ffc
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-11-02 00:25:58 +0530

    Refactor userModels.js to include password hashing for new and modified passwords

--- models/userModels.js
+++ models/userModels.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema(
 // Middleware to hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next(); // Only hash if password is new/modified
-
+  if (!this.isModified("password") && !this.isNew) return next(); // Only hash if password is new/modified
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
