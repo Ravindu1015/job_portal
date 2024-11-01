7a98cd753e4ec3203b05c85b2d4a21b2584ac7e0
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-11-02 00:07:44 +0530

    Add user routes to server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import connectDB from "./config/db.js";
 import testRoutes from "./routes/testRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 
 // dot env config
 dotenv.config();
@@ -41,6 +42,7 @@ app.post("/api/v1/test/test-post", (req, res) => {
 // Test Routes from external file
 app.use("/api/v1/test", testRoutes);
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
 
 //validation middleware
 app.use(errorMiddleware);
