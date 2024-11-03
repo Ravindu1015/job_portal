0acd8d89799626d51492b61a7b862eda0215193a
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-11-03 23:52:20 +0530

    Add authentication middleware to job creation route and update jobsController.js

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import userAuth from "../middlewares/authMiddleware.js";
+import { createJobController } from "../controllers/jobsController.js";
 
 const router = express.Router();
 
 //routes
 // create job||post
-router.post("/create-job", userAuth);
+router.post("/create-job", userAuth, createJobController);
 
 export default router;
