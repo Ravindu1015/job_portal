a5a8821cec0f48cd0733c1a8efa5dcde45ee4080
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-11-04 00:07:54 +0530

    Refactor createJobController to include createdBy field

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,1 +1,10 @@
-export const createJobController = async (req, res, next) => {};
+export const createJobController = async (req, res, next) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return next("Please provide all the fields");
+  }
+
+  req.body.createdBy = req.user.userId;
+  const job = await jobModels.create(req.body);
+  res.status(201).json({ job });
+};
