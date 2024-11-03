979db7b1720fc5c453e66002577f65bd43fe73ff
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-11-04 00:22:36 +0530

    Refactor createJobController to include createdBy field

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -8,4 +8,15 @@ const router = express.Router();
 // create job||post
 router.post("/create-job", userAuth, createJobController);
 
-export default router;
+//get jobs}}post
+const job=await jobsModel.create(req.body);
+res.status(201).json({job});
+
+
+export default getAllJobsController=async(req,res,next) =>{
+    const jobs=await jobsModel.find();
+    res.status(200).json({jobs}({
+        totalJobs=jobs.length,
+        jobs,
+    })
+}
