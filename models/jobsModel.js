d7d97d9e18c1578e29bce2d8768bcff6f8b9a618
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-11-03 23:41:56 +0530

    Add job model file

---
+++ models/jobsModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "Please provide a company name"],
+  },
+  position: {
+    type: String,
+    required: [true, "Please provide a position"],
+    minlength: 100,
+  },
+  status: {
+    type: String,
+    enum: ["interview", "declined", "pending"],
+    default: "pending",
+  },
+  workType: {
+    type: String,
+    enum: ["full-time", "part-time", "contract"],
+    default: "full-time",
+  },
+  workLocation: {
+    type: String,
+    default: "Mumbai",
+    required: [true, "work location required"],
+  },
+});
+
+export default mongoose.model("Job", jobSchema);
